509efad1de79408a92634c95b7993f1f096f1e45
Saransh-14 <saransh.sharma.7860@gmail.com>
2026-03-14 20:47:39 +0530

    created speaker module

--- speaker.py
+++ speaker.py
@@ -3,7 +3,7 @@ import pyttsx3 as tts
 engine = tts.init()
 engine.setProperty('rate', 170)
 voices = engine.getProperty('voices', )
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[0].id)
 
 def speak(text):
     engine.say(text)
